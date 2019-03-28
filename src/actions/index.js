e3dbd59bfb5c77b44f155be68247ee22a0f594ad
Haur Kang <haur.kang@gmail.com>
2019-03-28 21:57:15 +0000

    created an action creator - selectsong and imported into app.js

---
+++ src/actions/index.js
@@ -0,0 +1,10 @@
+// action creator
+
+export const selectSong = (song) => {
+    //return an action , type is required, payload is optional
+    return {
+        type: 'SONG_SELECTED',
+        payload: song
+    };
+};
+
--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,5 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import { selectSong } from '../actions';
 
 export default class App extends Component {
   render() {
