44ab2ee28970ddd5ce0065b946b564b6d02f15dd
Haur Kang <haur.kang@gmail.com>
2019-03-28 22:17:16 +0000

    created two new reducers

---
+++ src/reducers/index.js
@@ -0,0 +1,16 @@
+const songsReducer = () => {
+    return [
+        { title: 'Hello', duration: '5:00' },
+        { title: 'No promises', duration: '4:00' },
+        { title: 'Spice up your life', duration: '3:00' },
+        { title: 'Dancing in the rain', duration: '2:00' }
+    ];
+};
+
+const selectedSongReducer = (selectedSong = null, action) => {
+    if (action.type === 'SONG_SELECTED') {
+        return action.payload;
+    }
+
+    return selectedSong;
+};
