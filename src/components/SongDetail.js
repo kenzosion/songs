fe348bd99b243f7c3da6993f20f01b8047f0a23c
Haur Kang <haur.kang@gmail.com>
2019-03-29 14:17:45 +0000

    complete song detail component

--- src/components/SongDetail.js
+++ src/components/SongDetail.js
@@ -1,15 +1,24 @@
 import React from 'react';
 import { connect } from 'react-redux';
 
- const SongDetail = (props) => {
+ const SongDetail = ({song}) => {
+     if (!song)  {
+        return <p>Please select a song.</p>
+     }
   return (
     <div>
-      song detail
+     <h3>Details for:</h3>
+        <h5>
+            Title: {song.title}
+        </h5>
+        <p>
+            Duration: {song.duration}
+        </p>
     </div>
   )
 }
 
-const mapStateToProps = () => {
+const mapStateToProps = (state) => {
     return { song: state.selectedSong }
 }
 
